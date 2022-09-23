638edc5f4b7e280305d6acf3ad44009f99e8ec70
dev global <jaywoc12@gmail.com>
2022-09-22 23:09:22 -0400

    Moved readme to solution directory

---
+++ docs/html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -0,0 +1,6 @@
+var dir_68267d1309a1af8e8297ef4c3efbcdba =
+[
+    [ "example.cpp", "example_8cpp.html", "example_8cpp" ],
+    [ "Logger.cpp", "_logger_8cpp.html", "_logger_8cpp" ],
+    [ "Logger.hpp", "_logger_8hpp.html", "_logger_8hpp" ]
+];
--- docs/html/files_dup.js
+++ docs/html/files_dup.js
@@ -1,6 +1,4 @@
 var files_dup =
 [
-    [ "example.cpp", "example_8cpp.html", "example_8cpp" ],
-    [ "Logger.cpp", "_logger_8cpp.html", "_logger_8cpp" ],
-    [ "Logger.hpp", "_logger_8hpp.html", "_logger_8hpp" ]
+    [ "src", "dir_68267d1309a1af8e8297ef4c3efbcdba.html", "dir_68267d1309a1af8e8297ef4c3efbcdba" ]
 ];
